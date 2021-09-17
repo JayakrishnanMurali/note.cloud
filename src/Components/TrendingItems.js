195acee0ef69e02a0b785c1eed8c822c6c263dbe
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2021-09-18 00:37:44 +0530

    minor update

--- src/Components/TrendingItems.js
+++ src/Components/TrendingItems.js
@@ -18,11 +18,15 @@ function TrendingItems() {
               </h3>
             </div>
             <div className="flex-auto">
-              <h3 className="text-white">{trend.title}</h3>
-              <h6 className="text-gray-500">{trend.author}</h6>
+              <h3 className="text-white font-medium text-base">
+                {trend.title}
+              </h3>
+              <h6 className="text-gray-500 text-sm font-normal">
+                {trend.author}
+              </h6>
             </div>
             <div className="flex items-center">
-              <h6 className="text-gray-500">4:00</h6>
+              <h6 className="text-gray-500 text-base mr-2">4:00</h6>
               <div>
                 <h3 className="text-white">
                   <HiOutlineDotsVertical />
